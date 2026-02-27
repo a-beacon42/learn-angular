c8a59d01d3366a1e8ccf58457cde3f1c81fbff75
Alex Beacon <a.beacon42@gmail.com>
2026-02-27 16:49:43 -0500

    display recipes

--- tutorial/src/app/app.ts
+++ tutorial/src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecipeModel } from './models';
+import { MOCK_RECIPES } from './mock-recipes';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal<string>('My Recipe Box');
-  protected logSelection(selection: string): void {
-    console.log(selection);
+
+  protected readonly allRecipes = signal<RecipeModel[]>(MOCK_RECIPES);
+  protected readonly selectedRecipe = signal<RecipeModel>(MOCK_RECIPES[1]);
+
+  protected switchRecipe(newRecipeId: number): void {
+    this.selectedRecipe.set(MOCK_RECIPES[newRecipeId - 1]);
   }
 }
