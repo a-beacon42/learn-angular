2367564e80b489f7acb76d4582a101656389f62c
Alex Beacon <a.beacon42@gmail.com>
2026-02-28 10:43:31 -0500

    add servings; computed()

--- tutorial/src/app/app.ts
+++ tutorial/src/app/app.ts
@@ -1,11 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecipeModel } from './models';
 import { MOCK_RECIPES } from './mock-recipes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -14,8 +14,21 @@ export class App {
 
   protected readonly allRecipes = signal<RecipeModel[]>(MOCK_RECIPES);
   protected readonly selectedRecipe = signal<RecipeModel>(MOCK_RECIPES[1]);
+  protected readonly servings = signal<number>(1);
+
+  protected readonly adjustedIngredients = computed(() => (
+    this.selectedRecipe().ingredients.map(ingredient => ({ ...ingredient, quantity: ingredient.quantity * this.servings() }))
+  )
+  );
 
   protected switchRecipe(newRecipeId: number): void {
     this.selectedRecipe.set(MOCK_RECIPES[newRecipeId - 1]);
+  }
+
+  protected updateServings(servingsChange: number): void {
+    if (this.servings() === 1 && servingsChange === -1)
+      return;
+    else
+      this.servings.update(servings => servings + servingsChange);
   }
 }
